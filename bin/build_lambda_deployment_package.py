5bdfe312949b920c078700ed8832f2f8806b9e4b
Sanhe Hu <husanhe@gmail.com>
2022-12-10 11:58:39 -0500

    change lambda function entry point file name

--- bin/build_lambda_deployment_package.py
+++ bin/build_lambda_deployment_package.py
@@ -9,13 +9,13 @@ from pathlib import Path
 
 dir_project_root = Path(__file__).absolute().parent.parent
 path_requirements = dir_project_root / "requirements.txt"
-path_lambda_handler_py = dir_project_root / "lambda_handler.py"
+path_lambda_handler_py = dir_project_root / "lambda_function.py"
 
 dir_build = dir_project_root / "build"
 dir_build_lambda = dir_build / "lambda"
 dir_build_deployment_package = dir_build_lambda / "deploy"
 path_lambda_handler_in_deployment_package = (
-    dir_build_deployment_package / "lambda_handler.py"
+    dir_build_deployment_package / "lambda_function.py"
 )
 
 bin_pip = "pip"

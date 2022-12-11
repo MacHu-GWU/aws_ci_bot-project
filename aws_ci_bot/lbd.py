a69775cedd79eb8425e9e970a55b24687aa43ff9
Sanhe Hu <husanhe@gmail.com>
2022-12-11 15:58:08 -0500

    fix

--- aws_ci_bot/lbd.py
+++ aws_ci_bot/lbd.py
@@ -6,6 +6,7 @@ from aws_codecommit import CodeCommitEvent
 from aws_codebuild import CodeBuildEvent
 from boto_session_manager import BotoSesManager, AwsServiceEnum
 
+from .console import get_s3_console_url
 from .sns_event import (
     extract_sns_message_dict,
     upload_ci_event,
@@ -13,9 +14,9 @@ from .sns_event import (
 from .codecommit_and_codebuild import (
     CodeCommitEventHandler,
     CodeBuildEventHandler,
+    BuildJobRun,
 )
 from . import logger
-
 
 S3_BUCKET = os.environ["S3_BUCKET"]
 S3_PREFIX = os.environ["S3_PREFIX"]
@@ -38,29 +39,33 @@ def lambda_handler(event: dict, context: dict):
         ci_event = CodeCommitEvent.from_event(message_dict)
         ci_event.bsm = bsm
     elif message_dict["source"] == "aws.codebuild":
-        ci_event = CodeBuildEvent.from_event(message_dict)
+        ci_event = CodeBuildEvent.from_codebuid_notification_event(message_dict)
     else:  # pragma: no cover
         raise NotImplementedError
 
-    upload_ci_event(
+    s3_uri = upload_ci_event(
         s3_client=bsm.get_client(AwsServiceEnum.S3),
         event_dict=event,
         event_obj=ci_event,
         bucket=S3_BUCKET,
         prefix=S3_PREFIX,
     )
+    s3_console_url = get_s3_console_url(s3_uri=s3_uri)
 
     if isinstance(ci_event, CodeCommitEvent):
         cc_event_handler = CodeCommitEventHandler(
             bsm=bsm,
             cc_event=ci_event,
+            s3_console_url=s3_console_url,
         )
         cc_event_handler.execute()
     elif isinstance(ci_event, CodeBuildEvent):
-        cc_event_handler = CodeBuildEventHandler(
+        cb_event_handler = CodeBuildEventHandler(
             bsm=bsm,
             cb_event=ci_event,
+            s3_console_url=s3_console_url,
+            build_job_run=BuildJobRun.from_arn(ci_event.build_arn),
         )
-        cc_event_handler.execute()
+        cb_event_handler.execute()
     else:  # pragma: no cover
         raise NotImplementedError

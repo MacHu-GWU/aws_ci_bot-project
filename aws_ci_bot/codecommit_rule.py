54b7f6ae3d906821d429eb36f783391086f2ceb6
Sanhe Hu <husanhe@gmail.com>
2022-12-28 18:17:55 -0500

    Update codecommit_rule.py

--- aws_ci_bot/codecommit_rule.py
+++ aws_ci_bot/codecommit_rule.py
@@ -55,9 +55,9 @@ def check_what_to_do(cc_event: CodeCommitEvent) -> CodeCommitHandlerActionEnum:
             or cc_event.source_is_develop_branch
             or cc_event.source_is_fix_branch
             or cc_event.source_is_build_branch
-            or is_certain_semantic_branch(cc_event.source_branch, ["lbd", ])  # do Lambda Function stuff
+            or is_certain_semantic_branch(cc_event.source_branch, ["lbd", "lambda"])  # do Lambda Function stuff
             or is_certain_semantic_branch(cc_event.source_branch, ["layer",]) # build Lambda Layer
-            or is_certain_semantic_branch(cc_event.source_branch, ["cf", ]) # deploy CloudFormation stack
+            or is_certain_semantic_branch(cc_event.source_branch, ["cf", "cft", "cloudformation"]) # deploy CloudFormation stack
             or cc_event.source_is_doc_branch
             or cc_event.source_is_release_branch
         ):
